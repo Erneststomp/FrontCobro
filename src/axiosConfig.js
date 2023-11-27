3f6ed33f684e7fe23141824b30d45be974f5efec
Erneststomp <ernesto.lopez.lbm@outlook.com>
2023-11-27 13:49:45 -0600

    prueba

--- src/axiosConfig.js
+++ src/axiosConfig.js
@@ -1,7 +1,7 @@
 // axiosConfig.js
 import axios from 'axios';
 
-axios.defaults.baseURL = 'http://189.203.131.125/cobro/BackSistemaDeCobro/';
+axios.defaults.baseURL = 'https://189.203.131.125/cobro/BackSistemaDeCobro/';
 axios.defaults.headers.post['Content-Type'] = 'application/json';
 axios.defaults.withCredentials = true;
 
